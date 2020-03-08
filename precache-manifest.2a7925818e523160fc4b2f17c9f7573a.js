07d03c24b909f0b136f55f1ee677f3f8aafdb072
your name <your email>
2020-03-08 14:05:49 +0200

    Updates

---
+++ precache-manifest.2a7925818e523160fc4b2f17c9f7573a.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "df120dcff80a0cfc2255c3022cf80883",
+    "url": "/AbdAlhamid-fw/Programming-Company-Website/index.html"
+  },
+  {
+    "revision": "233395129d23c4fb3f83",
+    "url": "/AbdAlhamid-fw/Programming-Company-Website/static/css/main.0eae9705.chunk.css"
+  },
+  {
+    "revision": "951fc7e28f4a81352f34",
+    "url": "/AbdAlhamid-fw/Programming-Company-Website/static/js/2.7dbf07ea.chunk.js"
+  },
+  {
+    "revision": "928d7b5eb39d16fe9a880722c974b51e",
+    "url": "/AbdAlhamid-fw/Programming-Company-Website/static/js/2.7dbf07ea.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "233395129d23c4fb3f83",
+    "url": "/AbdAlhamid-fw/Programming-Company-Website/static/js/main.1bb6b69d.chunk.js"
+  },
+  {
+    "revision": "58593ab0057d2f393033",
+    "url": "/AbdAlhamid-fw/Programming-Company-Website/static/js/runtime-main.e38ceef4.js"
+  },
+  {
+    "revision": "8d614dee8fca5b96abc1b8eba38805f3",
+    "url": "/AbdAlhamid-fw/Programming-Company-Website/static/media/3.8d614dee.jpg"
+  }
+]);
